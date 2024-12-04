3d6a564560e78c50c98f57afb46c19717c5cbe98
krish vaghani <vaghanikrish1911@gmail.com>
2024-12-04 14:57:17 +0530

    added

--- src/routes/defaultLayout/defaultLayout.jsx
+++ src/routes/defaultLayout/defaultLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../../common/Header';
-import Footer from '../../common/Footer';
+import Header from '../../common/Header/index';
+import Footer from '../../common/Footer/index';
 
 export default function defaultLayout() {
     return (
